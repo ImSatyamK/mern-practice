26b1c743c26b185ac6b305a5019d1042241262f1
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-05-26 15:07:16 +0530

    Add router

--- products_store/backend/server.ts
+++ products_store/backend/server.ts
@@ -1,14 +1,19 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+import { productRouter } from './routes/product.route';
 
 dotenv.config();
 
 const app: express.Application = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response)=>{
-    res.json('Hello, World!');
-});
+app.use('/api/products', productRouter);
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 });
